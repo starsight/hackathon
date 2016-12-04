89b3586538488724c730430f54ec2161f9508b1d
zhangpengcheng <3029423192@qq.com>
2016-12-04 17:38:28 +0800

    zpc

--- android/Monitor/app/src/main/java/com/wenjiehe/monitor/MainActivity.java
+++ android/Monitor/app/src/main/java/com/wenjiehe/monitor/MainActivity.java
@@ -8,8 +8,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+//pc
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+
     }
 }
