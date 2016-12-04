7e3dbcbff35c8924e133cc623e02ccd5c7900973
zhangpengcheng <3029423192@qq.com>
2016-12-04 17:57:56 +0800

    zpc

--- android/Monitor/app/src/main/java/com/wenjiehe/monitor/MainActivity.java
+++ android/Monitor/app/src/main/java/com/wenjiehe/monitor/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+//zpccc
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
