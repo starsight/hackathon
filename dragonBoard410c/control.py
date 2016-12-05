5b1e1258d5a3bd3c3941ca2bc7ef2f7c04df92c3
linaro <172583172@qq.com>
2016-12-05 10:38:58 +0000

    control

--- dragonBoard410c/control.py
+++ dragonBoard410c/control.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
             if os.path.exists(file):
                 os.remove(file)
             #print "hello"
-            os.system("./control.sh")
+            os.system("sudo ./control.sh")
         time.sleep(1)
         i+=1
