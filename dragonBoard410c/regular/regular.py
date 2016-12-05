72a04169554d94fd13720249175a88cc546e28ce
linaro <172583172@qq.com>
2016-12-05 12:52:19 +0000

    add chmod in shell

--- dragonBoard410c/regular/regular.py
+++ dragonBoard410c/regular/regular.py
@@ -4,12 +4,13 @@ import time
 if __name__ == "__main__":
     i = 0
     while True:
-        if  i%30==0:
+        if  i%60==0:
             i = 0
             file = r'request'
             if os.path.exists(file):
                 os.remove(file)
             #print "hello"
+            os.system("chmod +x regular.sh")
             os.system("sudo ./regular.sh")
         time.sleep(1)
         i+=1
